5217a477fc70b6a1d1d8b329bb3500c80d2452f1
Dominik Otočan <dotocan@riviera.hr>
2023-07-28 13:37:49 +0200

    need to make the keyword pass to the script

--- scripts.js
+++ scripts.js
@@ -12,12 +12,19 @@ function injectThePublishScript () {
     })
 }
 
+// function that executes the publish script
+function injectTheDeleteScript () {
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        chrome.scripting.executeScript({ target: { tabId: tabs[0].id }, files: ['DeleteScript.js'] })
+    })
+}
+
 let boolOpenImages = false;
-let boolDownloadImages = false;
 
 // setting event handlers for upload and publish buttons
 document.getElementById('btnUploadEx').addEventListener('click', injectTheUploadScript)
 document.getElementById('btnPublishEx').addEventListener('click', injectThePublishScript)
+document.getElementById('btnDeleteEx').addEventListener('click', injectTheDeleteScript)
 
 // getting the initial state of the checkbox for opening all images
 try{
@@ -31,30 +38,10 @@ catch(error){
     console.log(error)
 }
 
-// getting the initial state of the checkbox for downloading the CSV file
-try{
-    chrome.storage.sync.get('chkDownloadImages', function(data) {
-        console.log('Checkbox state is ' + data.chkDownloadImages);
-        // Use data.checkboxState in your webpage-modifying code...
-        chkDownloadImages.checked = data.chkDownloadImages;
-    });
-}
-catch(error){
-    console.log(error)
-}
-
 // setting event handlers for the checkbox
 document.getElementById('chkOpenImages').addEventListener('change', (event) => {
     // Persist checkbox state in extension storage
     chrome.storage.sync.set({ 'chkOpenImages': event.target.checked }, function() {
-        console.log('Checkbox state is ' + event.target.checked);
-    });
-});
-
-// setting event handlers for the checkbox
-document.getElementById('chkDownloadImages').addEventListener('change', (event) => {
-    // Persist checkbox state in extension storage
-    chrome.storage.sync.set({ 'chkDownloadImages': event.target.checked }, function() {
         console.log('Checkbox state is ' + event.target.checked);
     });
 });
