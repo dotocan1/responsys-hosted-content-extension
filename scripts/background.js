fc08c7cc71656f8a8ceb3971f08f78da9c850afd
Dominik Otočan <49837535+dotocan1@users.noreply.github.com>
2023-11-08 10:27:35 +0100

    found a better event type

--- scripts/background.js
+++ scripts/background.js
@@ -22,5 +22,13 @@ console.log('script has been implemented')
 let elements = [];
 const DESIRED_URL = "https://interact.rsys8.net/interact/jsp/jindex.jsp";
 // Listen for DOMContentLoaded event to run the code when the page is loaded
-document.addEventListener('mousemove', removeDisabledAttributes);
+
+document.querySelectorAll('iframe').forEach(item => {
+    const fileElement = item.contentWindow.document.body.querySelectorAll('form');
+    fileElement.forEach((element) => {
+        element.addEventListener('mousemove', removeDisabledAttributes);
+    })
+});
+
+
 
