9970280648e4aaf09d3321c7a1cf9e3d359f9e18
Dominik Otočan <49837535+dotocan1@users.noreply.github.com>
2023-11-09 14:20:56 +0100

    rewrote the code to make it selectable through extension

--- scripts/content.js
+++ scripts/content.js
@@ -33,10 +33,31 @@ document.addEventListener('mousemove', attachEventListener);
 
 // TODO: Code that allows for multiple uploads
 
-let filesInput = document.getElementById('first_file_element');
-let filesInputsArray = filesInput.files;
+let filesInput;
+let filesInputsArray;
+let filesList;
+
+// get filesInput
+
+document.querySelectorAll('iframe').forEach(item => {
+    filesInput = item.contentWindow.document.body.getElementById('first_file_element');
+});
+
+// get filesInputsArray
+
+filesInputsArray = filesInput.files;
+
+// get count
+
 let count = filesInputsArray.length;
-let filesList = document.getElementById('files_list');
+
+// get filesList
+
+document.querySelectorAll('iframe').forEach(item => {
+    filesInput = item.contentWindow.document.body.getElementById('files_list');
+});
+
+// background work that creates new inputs that contain files
 
 for (let i = 0; i < filesInputsArray.length; i++) {
     let newInput = filesInput.cloneNode(true);
@@ -58,9 +79,3 @@ for (let i = 0; i < filesInputsArray.length; i++) {
     div.textContent = filesInputsArray[i].name;
     filesList.appendChild(div);
 }
-
-
-
-
-
-
