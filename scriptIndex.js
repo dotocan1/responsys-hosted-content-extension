c216fae88e92571eaeaeacddc26526c8dd16b558
Dominik Otočan <49837535+dotocan1@users.noreply.github.com>
2023-11-13 15:42:05 +0100

    making pictures clickable

---
+++ scriptIndex.js
@@ -0,0 +1,13 @@
+let images = document.querySelectorAll('img');
+images.forEach((image) => {
+    image.addEventListener('click', (event) => {
+        console.log("IMAGE CLICKED!")
+        console.log(event.target)
+
+        // do something with the image
+        // event.target.style.height = "1000px";
+        let img = event.target;
+
+        
+    })
+})
