a6d7e81a0af179e59c71e6e724a12a526267f327
Dominik Otočan <dominik.otocan1@gmail.com>
2024-01-22 22:24:23 +0100

    moving the window works in the extension as welll

--- content.js
+++ content.js
@@ -32,19 +32,48 @@
 // myFunction();
 
 console.log('Content script loaded.');
+
 // Your code here, which will run each time a webpage is loaded
 
-setTimeout(main, 1000);
+let editButtonEx;
 
-function main () {
-    let editButtonEx = document.querySelector('.uif-contentwidget-editPencilNew-white');;
-    console.log("editButtonEx Selected")
-    console.log(editButtonEx)
+function setEditButtonEx () {
+    editButtonEx = document.querySelector('.uif-contentwidget-editPencilNew-white');
     editButtonEx.addEventListener('click', () => {
         setTimeout(() => {
             let wd = document.querySelector('.uif-redwood-window-2g');
             wd.style.top = "0px";
             wd.style.left = "0px";
-        }, 1000);
+        }, 500);
     });
 }
+
+function delayedLoop (delay) {
+    let count = 0;
+
+    function iteration () {
+        console.log("Iteration:", count + 1);
+
+        // Increment the count
+        count++;
+
+        // Check if we have reached the desired number of iterations
+        if (editButtonEx === null || editButtonEx === undefined ) {
+            try {
+                setEditButtonEx();
+            } catch (error) {
+                console.log(error)
+            }
+            // Schedule the next iteration with the specified delay
+            setTimeout(iteration, delay);
+        } else {
+            console.log(editButtonEx)
+        }
+    }
+
+    // Start the first iteration
+    iteration();
+}
+
+// Example: Run the loop 5 times with a delay of 2 seconds (2000 milliseconds)
+delayedLoop(1000);
